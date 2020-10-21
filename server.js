eb944ea12b9148d829ff616f237a8839da6df0b7
Aidan T. Manning <periodicaidan@gmail.com>
2020-10-21 11:45:12 -0500

    Removed redeclaration of PORT

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 6996;
 
 const reservations = [];
-
-const PORT = process.env.PORT || 6969;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
