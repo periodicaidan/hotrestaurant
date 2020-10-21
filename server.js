fea956dfd87af92c596cbbe1f19ee5929889aeda
Aidan T. Manning <periodicaidan@gmail.com>
2020-10-21 10:57:01 -0500

    Added API endpoints

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 const app = express();
 
+const reservations = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, 'public/home.html');
     return res.sendFile(filePath);
@@ -16,6 +21,25 @@ app.get('/reserve', (req, res) => {
 app.get('/tables', (req, res) => {
     const filePath = path.join(__dirname, 'public/tables.html');
     return res.sendFile(filePath);
+});
+
+app.get('/api/tables', (req, res) => {
+    return res.json(reservations.slice(0, 5));
+});
+
+app.post('/api/tables', (req, res) => {
+    console.log(req.body);
+    const tablesAvailable = reservations.length < 5;
+    reservations.push(req.body);
+    return tablesAvailable;
+});
+
+app.get('/api/waitlist', (req, res) => {
+    return res.json(reservations.slice(5));
+});
+
+app.post('/api/clear', (req, res) => {
+    reservations.splice(0, reservations.length);
 });
 
 app.listen(6969);
